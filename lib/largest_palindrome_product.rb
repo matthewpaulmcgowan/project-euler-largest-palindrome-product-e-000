0b81f57c7f7982fc34b1b49b72155464160ce3c7
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2015-11-22 20:30:47 +0000

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+def largest_palindrome_product
+ count=0
+  b=[]
+  while count<10000
+    a=rand(1000)*rand(1000)
+    d=a.to_s.reverse.to_i
+    if a==d
+      b.push(a)
+      count=count+1
+    end
+  end
+  e=b.sort!
+  return e[e.length-1]
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,17 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+  def answer
+ count=0
+  b=[]
+  while count<2000
+    a=rand(1000)*rand(1000)
+    d=a.to_s.reverse.to_i
+    if a==d
+      b.push(a)
+      count=count+1
+    end
+  end
+  e=b.sort!
+  return e[e.length-1]
+end
+end
